90852d5aa151fa185d786212c339e4daa0095155
Sundar Sridharan <sundar.sridharan@gmail.com>
2014-03-10 14:49:24 -0700

    a generic cscope find and build C files

--- build_cscope.sh
+++ build_cscope.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# Created by Sundar Sridharan <sundar [dot] sridharan [@] gmail [dot] com>
+PWD=`pwd | sed 's/\//\\\\\//g'`
 rm cscope.files
 find . -name "*.h" > cscope.files.tmp
 find . -name "*.hh" >> cscope.files.tmp
 find . -name "*.c" >> cscope.files.tmp
 find . -name "*.cc" >> cscope.files.tmp
-cat cscope.files.tmp | grep -v " " | sed 's/^./\/Users\/sundar\/couchbase/g' >> cscope.files
+cat cscope.files.tmp | grep -v " " | sed "s/^./$PWD/g">> cscope.files
 cscope -b -i cscope.files
 rm cscope.files.tmp
