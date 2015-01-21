3dbe3aa46a3cc8ba63e539994db1e48e79ae80c3
Sundar Sridharan <sundar.sridharan@gmail.com>
2015-01-21 10:38:26 -0800

    let build_cscope put files in specified dir

--- build_cscope.sh
+++ build_cscope.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 # Created by Sundar Sridharan <sundar [dot] sridharan [@] gmail [dot] com>
+if [ $# -eq 0 ]
+    then
+        OUTPUT_DIR=.
+    else
+        OUTPUT_DIR=$1
+fi
+
 PWD=`pwd | sed 's/\//\\\\\//g'`
-rm cscope.files
-find . -name "*.h" > cscope.files.tmp
-find . -name "*.hh" >> cscope.files.tmp
-find . -name "*.c" >> cscope.files.tmp
-find . -name "*.cc" >> cscope.files.tmp
-cat cscope.files.tmp | grep -v " " | sed "s/^./$PWD/g">> cscope.files
+if [ -e $OUTPUT_DIR/cscope.files ]; then
+    rm $OUTPUT_DIR/cscope.files
+fi
+
+find . -name "*.h" > $OUTPUT_DIR/cscope.files.tmp
+find . -name "*.hh" >> $OUTPUT_DIR/cscope.files.tmp
+find . -name "*.c" >> $OUTPUT_DIR/cscope.files.tmp
+find . -name "*.cc" >> $OUTPUT_DIR/cscope.files.tmp
+cat $OUTPUT_DIR/cscope.files.tmp | grep -v " " | sed "s/^./$PWD/g">> $OUTPUT_DIR/cscope.files
+cd $OUTPUT_DIR
 cscope -b -i cscope.files
 rm cscope.files.tmp
